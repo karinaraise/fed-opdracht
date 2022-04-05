70029368ac65f35688e47af7930a3bcfc0cd118c
Wouter-Steenhuis <woutsteen.ws@gmail.com>
2022-04-05 15:59:40 +0200

    De geen poster hebben nu een afbeelding

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './MovieList.css';
+import Image from '../images/posterzonderposter.png';
 
-const MovieList = (props) => {
+function MovieList(props) {
+
+
     return (
         <>
             {props.movies.map((movie, index) => (
                 <div className='image-container'>
-                    <img src={movie.Poster} alt='movie' className='poster'></img>
+                   
+                    { movie.Poster !== 'N/A' ?  <img src={movie.Poster} alt='movie' className='poster'></img> : <img src={Image} alt='moviese' className='geenPoster'></img>}
+
                     <div className='overlay'></div>
                 </div>
             ))}
         </>
-    )
+    );
 }
 
 export default MovieList;
