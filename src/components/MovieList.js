445c76050fd234ef915b65880ff16ccb0eae41d8
Wouter-Steenhuis <woutsteen.ws@gmail.com>
2022-04-08 01:12:51 +0200

    alles bij elkaar
    moet nog gefixed worden

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,19 +2,21 @@ import React from 'react';
 import './MovieList.css';
 import Image from '../images/posterzonderposter.png';
 
-function MovieList(props) {
+const MovieList = (props) => {
+    const FavouriteComponent = props.favouriteComponent;
 
     return ( 
     <>
             {props.movies.map((movie, index) => (
                 <div className='image-container'>
-                   {/* <h3>{ movie.Title }</h3> */}
+                   
                     { movie.Poster !== 'N/A' ?  <img src={movie.Poster} alt='movie' className='poster'></img> : <img src={Image} alt='movie' className='geenPoster'></img>}
 
                     <div onClick={ ()=> props.handleFavouritesClick(movie)}className='overlay'>
-                        <h3 className='hoverText'>{movie.Title} <br></br> <br></br>{movie.Year}</h3>
+                       
                         <FavouriteComponent />
-                    </div>
+                    </div> 
+                    <h3 className='hoverText'>{movie.Title} <br></br> <br></br>{movie.Year}</h3>
                 </div>
             ))}
         </>
