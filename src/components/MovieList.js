4d4ede59c055a4486ed54b458ac71fbe3ebb5f82
Wouter-Steenhuis <woutsteen.ws@gmail.com>
2022-04-08 01:25:30 +0200

    titel jaar en favoriet staan

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -13,11 +13,14 @@ const MovieList = (props) => {
                     { movie.Poster !== 'N/A' ?  <img src={movie.Poster} alt='movie' className='poster'></img> : <img src={Image} alt='movie' className='geenPoster'></img>}
 
                     <div onClick={ ()=> props.handleFavouritesClick(movie)}className='overlay'>
-                       
+                       <h3 className='hoverText'>{movie.Title} <br></br> <br></br>{movie.Year}</h3>
                         <FavouriteComponent />
                     </div> 
-                    <h3 className='hoverText'>{movie.Title} <br></br> <br></br>{movie.Year}</h3>
+                    
                 </div>
+                
+                
+                
             ))}
         </>
     );
